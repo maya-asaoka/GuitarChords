708ff74e3a4becaaefcae40ba891a349b2176927
maya <mayamarina98@gmail.com>
2018-08-11 13:22:21 -0700

    Initial Commit

---
+++ GuitarChords/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GuitarChords
+//
+//  Created by Maya Asaoka on 2018-08-11.
+//  Copyright © 2018 Maya Asaoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
